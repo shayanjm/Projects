db7205c7955325a0300fc337dbc747b4ac2bc43b
Shayan Mohanty <shayan@evolucix.com>
2013-08-19 11:45:36 +0200

    Better... but still not working perfectly

--- Numbers/nextprimenumber.js
+++ Numbers/nextprimenumber.js
@@ -1,39 +1,58 @@
 // Next Prime number... Stupid implementation.
-var prompt = require("prompt");
-var async = require("async");
+var prompt = require('prompt');
 var first = 0;
 var next = 1;
-var on = true;
+var count = 1;
 
-prompt.start();
-async.whilst(
-  function () { return on; },
-  function (callback) {
-    async.series(
-      function (callback){
-        prompt.get({
-          properties: {
-            answer: {
-              description: "Do you want to see the next prime number?",
-              pattern:/(True|False)/,
-              message: "Answer must be True or False only."
-            }
-          }
-        }, function (err, result){
-          if (!(result.answer)){
-            process.exit(0);
-          }
-          while(next%2==0){
-            next++;
-          }
-          console.log("The previously calculated prime number was "+first+". The next prime number is"+next+".");
-        });
-        callback(null, next);
-      }
-      );
-    setTimeout(callback, 1);
-    },
-  function (err) {
-
+function prompting(){
+  prompt.get({
+  properties: {
+    answer: {
+      description: 'Do you want to see the next prime number?',
+      pattern:/(True|False)/,
+      message: 'Answer must be True or False only.'
+    }
   }
-);
+}, function (err, result){
+  // Accounting for the first number using a count. Probably not the best way.
+  if(result.answer === 'True'){
+    if (next === 1 && count !== 1){
+      next++;
+    }
+    
+    // Deciding if the number is prime or not.
+    while(next % 2 === 0){
+      while(next % 3 === 0){
+        next++;
+      }
+      next++;
+    }
+
+    // Grammatical changes because I am apparently OCD while coding.
+    if (count === 1){
+      console.log('The previously calculated prime number was '+first+'. the next prime number is '+next+'. You have asked to calculate prime numbers ' + count + ' time.');
+    }
+    else if (count > 1){
+      console.log('The previously calculated prime number was '+first+'. the next prime number is '+next+'. You have asked to calculate prime numbers ' + count + ' times.');
+    }
+
+    // Tidying up at the end.
+    first = next;
+    count++;
+    next++;
+
+    // Ghetto recursion.
+    prompted();
+  }
+  else{
+    // A clean end.
+    process.exit(0);
+  }
+});
+}
+// Dat recursion
+function prompted(){
+  prompting();
+}
+// Genesis
+prompted();
